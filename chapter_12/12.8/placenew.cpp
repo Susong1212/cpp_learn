848636c42ae04093249451cad606137c865f03c6
sxs <xiaosongsu1212@gmail.com>
2022-08-15 10:18:48 +0800

    add new delete

--- chapter_12/12.8/placenew.cpp
+++ chapter_12/12.8/placenew.cpp
@@ -19,13 +19,29 @@ class JustTesting
 const int BUF = 512;
 int main(void)
 {
-    char* buffer = new char[BUF]; 
+    char* buffer = new char[BUF];
+
     JustTesting* pc1, * pc2;
+
     pc1 = new(buffer) JustTesting;
     pc2 = new JustTesting("sxs", 24);
 
-    cout << "buffer: " << (char*)buffer << endl;
+    cout << "buffer: " << (void*)buffer << endl;
     cout << "sxs: " << pc2 << endl;
+
+    cout << pc1 << ": " ;
+    pc1 -> Show();
+    cout << pc2 << ": ";
+    pc2 -> Show();
+
+    JustTesting* pc3, * pc4;
+    pc3 = new(buffer) JustTesting("Bad Idea", 6);
+    pc4 = new JustTesting("Head2", 10);
+
+    cout << pc3 << ": ";
+    pc3 -> Show();
+    cout << pc4 << ": ";
+    pc4 -> Show();
 
     return 0;
 }
