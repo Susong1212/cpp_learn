58ebc6782dafd604db48ee153f1b5d34e8518ef0
sxs <xiaosongsu1212@gmail.com>
2022-08-15 11:14:52 +0800

    modify lauch.json

--- chapter_12/12.8/placenew.cpp
+++ chapter_12/12.8/placenew.cpp
@@ -1,47 +1,60 @@
-#include<iostream>
-#include<string>
-#include<new>
+#include <iostream>
+#include <string>
+#include <new>
 
 using namespace std;
 
 class JustTesting
 {
-    private:
-        string words;
-        int number;
-    public:
-        JustTesting(const string &s = "Just testing", int n = 0)
-        {words = s; number = n; cout << words << " constructed\n"; }
-        ~JustTesting(){cout << words << "destroyde\n";}
-        void Show() const {cout << words << " , " << number << endl;}
+private:
+    string words;
+    int number;
+
+public:
+    JustTesting(const string &s = "Just testing", int n = 0)
+    {
+        words = s;
+        number = n;
+        cout << words << " constructed\n";
+    }
+    ~JustTesting() { cout << words << " destroyde\n"; }
+    void Show() const { cout << words << " , " << number << endl; }
 };
 
 const int BUF = 512;
 int main(void)
 {
-    char* buffer = new char[BUF];
+    char *buffer = new char[BUF];
 
-    JustTesting* pc1, * pc2;
+    JustTesting *pc1, *pc2;
 
-    pc1 = new(buffer) JustTesting;
+    pc1 = new (buffer) JustTesting;
     pc2 = new JustTesting("sxs", 24);
 
-    cout << "buffer: " << (void*)buffer << endl;
+    cout << "buffer: " << (void *)buffer << endl;
     cout << "sxs: " << pc2 << endl;
 
-    cout << pc1 << ": " ;
-    pc1 -> Show();
+    cout << pc1 << ": ";
+    pc1->Show();
     cout << pc2 << ": ";
-    pc2 -> Show();
+    pc2->Show();
 
-    JustTesting* pc3, * pc4;
-    pc3 = new(buffer) JustTesting("Bad Idea", 6);
-    pc4 = new JustTesting("Head2", 10);
+    JustTesting *pc3, *pc4;
+    pc3 = new (buffer) JustTesting("Bad Idea", 6);
+    pc4 = new JustTesting("hjy", 10);
 
     cout << pc3 << ": ";
-    pc3 -> Show();
+    pc3->Show();
     cout << pc4 << ": ";
-    pc4 -> Show();
+    pc4->Show();
+
+    delete pc2;
+    delete pc4;
+
+    // delete pc1;
+    // delete pc3;//delete 不能与定位new运算符配合使用
+
+    delete[] buffer;
 
     return 0;
 }
