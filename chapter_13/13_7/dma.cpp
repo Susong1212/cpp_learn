c9fccd3615e84ba2fd86424fdb1bd5e32dc7a34e
sxs <xiaosongsu1212@gmail.com>
2022-08-16 14:48:50 +0800

    fomat code

--- chapter_13/13_7/dma.cpp
+++ chapter_13/13_7/dma.cpp
@@ -3,100 +3,102 @@
 
 baseDMA::baseDMA(const char *l, int r)
 {
-	label = new char[strlen(l) + 1];
-	strcpy(label, l);
-	rating = r;
+    label = new char[strlen(l) + 1];
+    strcpy(label, l);
+    rating = r;
 }
 
 baseDMA::baseDMA(const baseDMA &rs)
 {
-	label = new char[strlen(rs.label) + 1];
-	strcpy(label, rs.label);
-	rating = rs.rating;
+    label = new char[strlen(rs.label) + 1];
+    strcpy(label, rs.label);
+    rating = rs.rating;
 }
 
 baseDMA::~baseDMA()
 {
-	delete []label;
+    delete[] label;
 }
 
 baseDMA &baseDMA::operator=(const baseDMA &rs)
 {
-	if(this == &rs)
-		return *this;
-	delete []label;
-	label = new char[strlen(rs.label) + 1];
-	strcpy(label, rs.label);
-	rating = rs.rating;
-	return *this;
+    if (this == &rs)
+        return *this;
+    delete[] label;
+    label = new char[strlen(rs.label) + 1];
+    strcpy(label, rs.label);
+    rating = rs.rating;
+    return *this;
 }
 
 ostream &operator<<(ostream &os, const baseDMA &rs)
 {
-	os << "Label: " << rs.label << endl;
-	os << "Rating : " << rs.rating << endl;
+    os << "Label: " << rs.label << endl;
+    os << "Rating : " << rs.rating << endl;
+    return os;
 }
 
 lacksDMA::lacksDMA(const char *l, int r, const char *c) : baseDMA(l, r)
 {
-	strncpy(color, c, COL_LEN - 1);
-	color[COL_LEN - 1] = '\0';
+    strncpy(color, c, COL_LEN - 1);
+    color[COL_LEN - 1] = '\0';
 }
 
 lacksDMA::lacksDMA(const baseDMA &rs, const char *c) : baseDMA(rs)
 {
-	strncpy(color, c, COL_LEN - 1);
-	color[COL_LEN - 1] = '\0';
+    strncpy(color, c, COL_LEN - 1);
+    color[COL_LEN - 1] = '\0';
 }
 
 ostream &operator<<(ostream &os, const lacksDMA &ls)
 {
-	os << (const baseDMA &)ls;
-	os << "Color: " << ls.color << endl;
+    os << (const baseDMA &)ls; //将ls强制类型转换为基类
+    os << "Color: " << ls.color << endl;
+    return os;
 }
 
 hasDMA::hasDMA(const char *l, int r, const char *s) : baseDMA(l, r)
 {
-	style = new char[strlen(s) + 1];
-	strcpy(style, s);
+    style = new char[strlen(s) + 1];
+    strcpy(style, s);
 }
 
 hasDMA::hasDMA(const baseDMA &rs, const char *s) : baseDMA(rs)
 {
-	style = new char[strlen(s) + 1];
-	strcpy(style, s);
+    style = new char[strlen(s) + 1];
+    strcpy(style, s);
 }
 
 hasDMA::hasDMA(const hasDMA &hs) : baseDMA(hs)
 {
-	cout << "Copy construct function." << endl;
+    cout << "Copy construct function." << endl;
 
-	style = new char[strlen(hs.style) + 1];
-	strcpy(style, hs.style);
+    style = new char[strlen(hs.style) + 1];
+    strcpy(style, hs.style);
 }
 
 hasDMA::~hasDMA()
 {
-	delete []style;
+    delete[] style;
 }
 
 hasDMA &hasDMA::operator=(const hasDMA &hs)
 {
-	cout << "operator function." << endl;
+    cout << "operator function." << endl;
 
-	if(this == &hs)
-		return *this;
+    if (this == &hs)
+        return *this;
 
-	baseDMA::operator=(hs);//*this = hs;
-	delete []style;
-	style = new char[strlen(hs.style) + 1];
-	strcpy(style, hs.style);
-	return *this;
+    baseDMA::operator=(hs); //*this = hs;
+    delete[] style;
+    style = new char[strlen(hs.style) + 1];
+    strcpy(style, hs.style);
+    return *this;
 }
 
 ostream &operator<<(ostream &os, const hasDMA &hs)
 {
-	os << (const baseDMA &)hs;
-	os << "Style: " << hs.style << endl;
-	return os;
+    os << (const baseDMA &)hs;//强制转换为基类
+    os << "Style: " << hs.style << endl;
+    return os;
 }
