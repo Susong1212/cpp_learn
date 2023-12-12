218aaf075f58067f832272acc1ab73fc1e7197b8
sxs <xiaosongsu1212@gmail.com>
2023-12-12 09:32:31 +0800

    test libfiles

---
+++ testcpp/libtest.h
@@ -0,0 +1,6 @@
+#ifndef LIBSAYHELLO_H_
+#define LIBSAYHELLO_H_
+
+void sayhello();
+
+#endif
---
+++ testcpp/usetest.cpp
@@ -0,0 +1,8 @@
+#include "libtest.h"
+
+using namespace std;
+
+int main() {
+    sayhello();
+    return 0;
+}
